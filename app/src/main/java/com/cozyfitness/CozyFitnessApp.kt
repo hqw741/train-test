3168db280d00f0e49a64bc133fa0a64e36eb0eb7
P296731 <CGN@PTS.001>
2026-05-29 14:33:02 +0800

    refactor: CozyFitnessApp initializes default data on startup

--- app/src/main/java/com/cozyfitness/CozyFitnessApp.kt
+++ app/src/main/java/com/cozyfitness/CozyFitnessApp.kt
@@ -1,7 +1,22 @@
 package com.cozyfitness
 
 import android.app.Application
+import com.cozyfitness.data.local.DefaultDataInitializer
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Inject
 
 @HiltAndroidApp
-class CozyFitnessApp : Application()
+class CozyFitnessApp : Application() {
+
+    @Inject
+    lateinit var defaultDataInitializer: DefaultDataInitializer
+
+    private val applicationScope = CoroutineScope(SupervisorJob())
+
+    override fun onCreate() {
+        super.onCreate()
+        defaultDataInitializer.initializeIfNeeded(applicationScope)
+    }
+}
